73eebe18c9b763b52a10e322e40dc2ffe91d7b33
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-02 10:26:55 -0700

    fix some problem on the code in js

---
+++ app.js
@@ -0,0 +1,27 @@
+var buttonTranslate = document.querySelector("#button-txt");
+var txtInputs = document.querySelector("#input-txts");
+var divOutputs = document.querySelector("#outputs");
+ 
+var serverURL ="https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
+}
+function errorHandler(error) {
+    alert("something wrong with server!,please try after sometime..");
+}
+function clickHandler() {
+    
+    var inputText = txtInputs.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        divOutput.innerText = translatedText;
+
+    })
+    .catch(errorHandler);
+}
+
+
+buttonTranslate.addEventListener("click",clickHandler);
